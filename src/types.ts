a04a8a13520b3d779cada462b843c828a0a174d9
Arthur Park <arthurh.park@gmail.com>
2021-07-23 21:59:12 -0700

    Fix lint script

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
 export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
-export type XOR<T, U> = T | U extends object
+export type XOR<T, U> = T | U extends Record<string, unknown>
   ? (Without<T, U> & U) | (Without<U, T> & T)
   : T | U;
