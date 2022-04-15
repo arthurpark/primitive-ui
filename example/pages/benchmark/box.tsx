15839158c46032c1d6fed45d34ec98fff933e63e
Arthur Park <arthurh.park@gmail.com>
2022-04-14 20:49:20 -0700

    Show render duration in UI

--- example/pages/benchmark/box.tsx
+++ example/pages/benchmark/box.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { Tree } from 'components/Benchmark/BoxTree';
 
 export default function BoxTree(): JSX.Element {
+  const [measurements, setMeasurements] = React.useState([]);
+
   if (typeof window !== 'undefined') {
     window.performance.mark('BeginBoxTree');
   }
@@ -11,8 +13,17 @@ export default function BoxTree(): JSX.Element {
 
     window.performance.measure('measure', 'BeginBoxTree', 'EndBoxTree');
 
-    console.log(window.performance.getEntriesByName('measure'));
+    setMeasurements(window.performance.getEntriesByName('measure'));
   }, []);
 
-  return <Tree depth={17} />;
+  return (
+    <>
+      <Tree depth={17} />
+
+      <div className="fixed z-10 top-20 right-5 inset-x-0 shadow text-right">
+        Duration:{' '}
+        {measurements.length > 0 ? measurements[0].duration.toFixed(2) : ''} ms
+      </div>
+    </>
+  );
 }
