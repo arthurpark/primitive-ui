73bf388191a7f35ce6bc299ec44b3cd4720106a0
Arthur Park <arthurh.park@gmail.com>
2024-11-01 16:15:10 -0700

    Add gap support for flexbox

--- src/Layout/FlexBox.tsx
+++ src/Layout/FlexBox.tsx
@@ -1,7 +1,7 @@
 import cx from 'clsx';
 import React from 'react';
 import { Box, BoxProps } from '../Box';
-import { Direction, Justify, Align, Wrap } from './types';
+import { Direction, Justify, Align, Wrap, Gap } from './types';
 import { Responsive, responsive, createPrefixValueResolver } from '../utils';
 
 export type FlexBoxProps = BoxProps & {
@@ -9,10 +9,12 @@ export type FlexBoxProps = BoxProps & {
   justify?: Responsive<Justify>;
   align?: Responsive<Align>;
   wrap?: Responsive<Wrap>;
+  gap?: Responsive<Gap>;
 };
 
 export function FlexBox(props: FlexBoxProps): JSX.Element {
-  const { className, direction, align, justify, wrap, ...boxProps } = props;
+  const { className, direction, align, justify, wrap, gap, ...boxProps } =
+    props;
 
   const flexDirectionClassNames = responsive(
     createPrefixValueResolver('flex'),
@@ -31,6 +33,8 @@ export function FlexBox(props: FlexBoxProps): JSX.Element {
     wrap
   );
 
+  const gapClassNames = responsive(createPrefixValueResolver('gap'), gap);
+
   return (
     <Box
       className={cx(
@@ -39,6 +43,7 @@ export function FlexBox(props: FlexBoxProps): JSX.Element {
         justifyContentClassNames,
         alignItemsClassNames,
         flexWrapClassNames,
+        gapClassNames,
         className
       )}
       {...boxProps}
--- src/Layout/types.ts
+++ src/Layout/types.ts
@@ -1,3 +1,5 @@
+import { Scale, Pixel } from '../tokens/scale';
+
 export type Direction = 'row' | 'row-reverse' | 'col' | 'col-reverse';
 
 // Use literal string because this will also be used by non elements
@@ -11,3 +13,4 @@ export type Wrap = 'no-wrap' | 'wrap' | 'wrap-reverse';
 export type Flex = 'none' | 1 | 'auto' | 'initial';
 export type Grow = 'grow' | 'grow-0';
 export type Shrink = 'shrink' | 'shrink-0';
+export type Gap = Pixel | Scale;
